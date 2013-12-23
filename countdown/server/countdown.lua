3179f8d99aaaa07747da0a20b29052ba180fae4d
Eraknelo <rene.sackers@live.com>
2013-12-23 13:30:01 +0100

    Classified the script to resolve possible conflicts with variables.

--- countdown/server/countdown.lua
+++ countdown/server/countdown.lua
@@ -1,14 +1,26 @@
-local textColor = Color(255, 0, 0)
-local activeCountdowns = false
-local countdownTimer = Timer()
-local secondsToGo = 4
-local timeOfLastCount = 0
-local tickSubscription
+class 'Countdown'
 
-local timeouts = {}
-local playerTimeout = 30
+function Countdown:__init()
+	-- Variables
+	self.textColor = Color(255, 0, 0)
+	self.activeCountdowns = false
+	self.countdownTimer = Timer()
+	self.secondsToGo = 4
+	self.timeOfLastCount = 0
+	self.tickSubscription = nil
+	self.timeouts = {}
+	
+	-- Yout can edit these variables
+	self.countdownMin = 3
+	self.countdownMax = 5
+	self.countdownDefault = self.countdownMin
+	self.playerTimeout = 30
+	
+	-- Events
+	Events:Subscribe("PlayerChat", self, self.PlayerChat)
+end
 
-Events:Subscribe("PlayerChat", function (args)
+function Countdown:PlayerChat(args)
 	local commands = {}
 	for command in string.gmatch(args.text, "[^%s]+") do
 		table.insert(commands, command)
@@ -16,60 +28,67 @@ Events:Subscribe("PlayerChat", function (args)
 	
 	if commands[1] ~= "/countdown" then return true end
 	
-	if activeCountdowns then
-		Chat:Send(args.player, "Countdown in progress.", textColor)
+	if self.activeCountdowns then
+		Chat:Send(args.player, "Countdown in progress.", self.textColor)
 		return false
 	end
 	
 	if commands[2] ~= nil then
 		local secondsCommandNumber = tonumber(commands[2])
 		if secondsCommandNumber == nil then
-			Chat:Send(args.player, "That is not a valid number.", textColor)
+			Chat:Send(args.player, "That is not a valid number.", self.textColor)
 			return false
-		elseif secondsCommandNumber < 3 or secondsCommandNumber > 5 then
-			Chat:Send(args.player, "You can only specify a number from 3 to 5 seconds.", textColor)
+		elseif secondsCommandNumber < self.countdownMin or secondsCommandNumber > self.countdownMax then
+			Chat:Send(args.player, "You can only specify a number from " .. self.countdownMin .. " to " .. self.countdownMax .. " seconds.", self.textColor)
 			return false
 		end
-		secondsToGo = secondsCommandNumber + 1
+		self.secondsToGo = secondsCommandNumber
 	else
-		secondsToGo = 4
+		self.secondsToGo = self.countdownDefault
 	end
 	
-	local seconds = countdownTimer:GetSeconds()
+	local seconds = self.countdownTimer:GetSeconds()
 	
-	local timeout = timeouts[args.player:GetId()]
+	local timeout = self.timeouts[args.player:GetId()]
 	if timeout ~= nil then
-		if seconds - timeout < playerTimeout then
-			Chat:Send(args.player, "You can only countdown once every " .. playerTimeout .. " seconds.", textColor)
+		if seconds - timeout < self.playerTimeout then
+			Chat:Send(args.player, "You can only countdown once every " .. self.playerTimeout .. " seconds.", self.textColor)
 			return false
 		end
 	end
 	
-	timeouts[args.player:GetId()] = seconds
-	activeCountdowns = true
-	timeOfLastCount = countdownTimer:GetMilliseconds()
+	self.timeouts[args.player:GetId()] = seconds
+	self.activeCountdowns = true
+	self.timeOfLastCount = 0
 	
-	tickSubscription = Events:Subscribe("PreTick", function (args)
-		local milliseconds = countdownTimer:GetMilliseconds()
-		if milliseconds - timeOfLastCount >= 1000 then
-			secondsToGo = secondsToGo - 1
-			if secondsToGo < 0 then
-				activeCountdowns = false
-				return
-			end
-		
-			timeOfLastCount = milliseconds
-			
-			if secondsToGo > 0 then
-				Chat:Broadcast("Countdown: " .. tostring(secondsToGo), textColor)
-			else
-				Chat:Broadcast("Countdown: GO!", textColor)
-				Events:Unsubscribe(tickSubscription)
-				activeCountdowns = false
-			end
-		end
-	end)
+	self.tickSubscription = Events:Subscribe("PreTick", self, self.PreTick)
 	
 	return false
-end)
+end
 
+function Countdown:PreTick(args)
+	local milliseconds = self.countdownTimer:GetMilliseconds()
+	if milliseconds - self.timeOfLastCount >= 1000 then
+		if self.secondsToGo < 0 then
+			self.activeCountdowns = false
+			if self.tickSubscription ~= nil then
+				Events:Unsubscribe(self.tickSubscription)
+			end
+			return
+		end
+	
+		self.timeOfLastCount = milliseconds
+		
+		if self.secondsToGo > 0 then
+			Chat:Broadcast("Countdown: " .. tostring(self.secondsToGo), self.textColor)
+		else
+			Chat:Broadcast("Countdown: GO!", self.textColor)
+			Events:Unsubscribe(self.tickSubscription)
+			self.activeCountdowns = false
+		end
+		
+		self.secondsToGo = self.secondsToGo - 1
+	end
+end
+
+local countdown = Countdown()
